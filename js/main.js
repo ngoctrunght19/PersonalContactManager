0424e4fb57b19629dd38bec554cb52c57943b9b8
ngoctrunght19 <ngoctrunght19@gmail.com>
2016-11-19 22:42:54 +0700

    tìm kiếm được rồi

--- js/main.js
+++ js/main.js
@@ -2,4 +2,15 @@ $(document).ready(function(){
 	var browserHeight = window.innerHeight; //lấy chiều cao khung trình duyệt trên các trình duyệt khác
 	var ContentHeight = browserHeight - 150;//tính chiều cao cho phần nội dung
 	$('.listbox').css({"height": ContentHeight});
+	$('#search').keyup(function(){
+		var input = $(this).val().toUpperCase();
+	    $('#contactUL li').each(function () {
+	    	var temp = $(this).children('a').text().toUpperCase();
+	        if (temp.search(input) > -1) {
+	            $(this).show();
+	        } else {
+	            $(this).hide();
+	        }
+	    });
+	});
 });
